de62ae072887bec2110ad2bc9cd1c2bb95c717ba
laini-creator <a29833540@gmail.com>
2025-12-03 12:07:12 +0800

    add entry page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-import EntryPage from "./pages/EntryPage";
+import "./App.css";
 
 export default function App() {
-  return <EntryPage />;
+  return (
+    <div className="container">
+      <h1>歡迎使用 e-Coop 系統</h1>
+      <p>請選擇身份進入系統：</p>
+
+      <div className="btn-group">
+        <a className="btn student" href="https://e-coop-stu.github.io/e-coop-stu/">
+          進入學生端（Student）
+        </a>
+
+        <a className="btn admin" href="https://e-coop-stu.github.io/coop-admin/">
+          進入合作社端（Admin）
+        </a>
+      </div>
+    </div>
+  );
 }
