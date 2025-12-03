e7e47cae9619efb61ebc33f7f99799cf8f4a7c2b
laini-creator <a29833540@gmail.com>
2025-12-03 12:09:36 +0800

    add entry page

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ export default function App() {
       <p>請選擇身份進入系統：</p>
 
       <div className="btn-group">
-        <a className="btn student" href="https://e-coop-stu.github.io/e-coop-stu/">
+        <a className="btn student" href="https://e-coop-stu.github.io/stu/">
           進入學生端（Student）
         </a>
 
-        <a className="btn admin" href="https://e-coop-stu.github.io/coop-admin/">
+        <a className="btn admin" href="https://e-coop-stu.github.io/coop/">
           進入合作社端（Admin）
         </a>
       </div>
